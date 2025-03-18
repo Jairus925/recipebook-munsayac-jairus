e39b7801f8c00904b46fd581b046868f393c08a9
Jairus Munsayac <jcpmunsayac@gmail.com>
2025-03-18 20:34:30 +0800

    Added Profile Model Admin code in admin and centered contents in login template

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient, Ingredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Recipe, RecipeIngredient, Ingredient, Profile
 # Register your models here.
 
 class RecipeAdmin(admin.ModelAdmin):
@@ -14,6 +17,15 @@ class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
     list_display = ('ingredient', 'recipe', 'quantity') 
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,] 
+
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
