7f14901d11d7033f67e5a65c99aa316e9105adbf
Jairus Munsayac <jcpmunsayac@gmail.com>
2025-04-07 19:42:03 +0800

    Added RecipeImage model and made migrations

--- ledger/models.py
+++ ledger/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
@@ -42,3 +40,8 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.ingredient} for {self.recipe}"
+    
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to="uploads/%Y/%m/%d/")
+    description = models.CharField(max_length=255)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="images")
